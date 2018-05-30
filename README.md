97661f04c54857dfbeb5343bc85fa474c1ff4cde
Akmal Darari <akmalflightsimmer@gmail.com>
2018-05-30 14:08:01 +0700

    Semuanyaaaaaaaa

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# FP_Otomata_A_Team5
-FP Mata kuliah Otomata A 2017-2018 Departemen Informatika ITS. Dari kelompok 5
+# FP_Otomata_A_1718_Team5
+FP Mata kuliah Otomata A 2017-2018 Departemen Informatika ITS. Dari Kelompok 5
+
+Anggota Kelompok:
+1. Deddy Aditya P/5116100069
+2. M. Ilham Akbar S/5116100114
+3. Akmal Darari R.B/5116100148
+4. Rifqi Mukti Wicaksana/5116100xxx
+
+Deskripsi:
+Membuat Program Lexical Analyzer. Untuk menganalisis bahasa PHP
---
+++ StrukturbahasaPHP.php
@@ -0,0 +1,48 @@
+<?php
+    $var1 = 20; //kalo int
+    $var4 = 5.4;
+    $var2 = "apa"; //kalo string
+    $var3 = array("apa", "$var1", "apa");
+
+    if($var4==5) {
+              echo $var4;
+     }
+    elseif($var4==4) { die();
+    }
+    else $var4 = 8 * 4;
+
+    for ($x = 0; $x <= 10; $x++) {
+         echo "Hello world";
+         static $y = 9;
+    }
+
+    $array = ($i, $a, $b)
+    foreach($array as $value) {
+             $var = $value['a'];
+    }
+
+    echo $value; //sama kayak printf
+    echo "Deddy" . $var . "senpai"; //sama kayak atas
+
+   $x = $y + $u;
+
+   function senpai ($a) {
+       return $a - 15;
+    }
+
+   $a = $y / 5;
+   $b = $d / $a;
+
+   senpai();
+
+  while(true) {
+       echo "Deddy senpaaai";
+       break;
+  }
+?>
+/**
+ * Created by PhpStorm.
+ * User: K1 0 01 19 SBI
+ * Date: 30/05/2018
+ * Time: 13.47
+ */
